2dfba45c9293946bbfe65977e67e3467cebed419
Vinicio Brejinski <viniciosdb@gmail.com>
2023-04-18 20:29:41 -0300

    initial commit

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, ProjectsView, ProjectView, PortfoliosView, PortfolioView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("projects", ProjectsView.as_view(), name="projects"),
+    path("projects/<int:pk>", ProjectView.as_view(), name="project"),
+    path("portfolios", PortfoliosView.as_view(), name="portfolios"),
+    path("portfolios/<int:pk>", PortfolioView.as_view(), name="portfolio"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,15 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+class ProjectsView(TemplateView):
+    template_name = "projects.html"
+
+class ProjectView(TemplateView):
+    template_name = "project.html"
+
+class PortfoliosView(TemplateView):
+    template_name = "portfolios.html"
+
+class PortfolioView(TemplateView):
+    template_name = "portfolio.html"
